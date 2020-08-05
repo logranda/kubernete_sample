0f7cf515012cce4f86f40c4d332fb8ef31dcbbdc
lgranda <lgranda@rapicredit.com.co>
2020-08-05 16:59:00 -0500

    master se agregan los artefactos

--- src/main/java/com/example/demo/controller/testController.java
+++ src/main/java/com/example/demo/controller/testController.java
@@ -2,16 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.config.PropertiesConfig;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +31,15 @@ public class testController {
         return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 
+    @GetMapping("/document/{ruta}")
+    public String getDocument(@PathVariable("ruta") String ruta) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(ruta + "application.yml"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        return br.toString();
+    }
 
     @Getter
     @Setter
@@ -40,16 +48,6 @@ public class testController {
         private Integer id;
         private String place;
         private String value;
-    }
-
-    @GetMapping("/document")
-    public String getDocument() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("/usr/apps/application.yml"));
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
-        return br.toString();
     }
 
 }
